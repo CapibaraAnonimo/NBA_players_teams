68a140ccf6c573b45a3e6241af788c489f66bb2a
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-20 12:14:03 +0200

    Eliminado un import duplicado en app.module.ts

--- nba_player_teams/src/app/app.module.ts
+++ nba_player_teams/src/app/app.module.ts
@@ -1,14 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TeamsComponent } from './components/teams/teams.component';
-import { MaterialImportsModule } from "./modules/material-imports.module";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { TeamsComponent } from "./components/teams/teams.component";
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TeamsComponent} from './components/teams/teams.component';
+import {MaterialImportsModule} from "./modules/material-imports.module";
+import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
@@ -26,4 +25,5 @@ import { TeamsComponent } from "./components/teams/teams.component";
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
