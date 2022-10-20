a4b267adc54e1d6efde807ea711b15d5ae513325
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-20 13:06:22 +0200

    Hecha el servicio de equipos

---
+++ nba_player_teams/src/app/services/teams.service.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {TeamsResponse} from "../interfaces/teams.interface";
+import {environment} from "../../environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TeamsService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getTeams(year: number): Observable<TeamsResponse> {
+    return this.http.get<TeamsResponse>(`${environment.baseUrl}/data/10s/prod/v1/${year}/team.json`);
+  }
+}
