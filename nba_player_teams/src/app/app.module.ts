5824266f936454064709fd25f59e30db9407b051
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-20 10:51:30 +0200

    Añadidos módulos

--- nba_player_teams/src/app/app.module.ts
+++ nba_player_teams/src/app/app.module.ts
@@ -4,15 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TeamsComponent } from './components/teams/teams.component';
+import { MaterialImportsModule } from "./modules/material-imports.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialImportsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
