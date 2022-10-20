17888ff64b5bd0e9be1b72c62ddd5d2e56ef3dc5
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-20 10:55:41 +0200

    Creados los componentes

--- nba_player_teams/src/app/app.module.ts
+++ nba_player_teams/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TeamsComponent } from "./components/teams/teams.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
