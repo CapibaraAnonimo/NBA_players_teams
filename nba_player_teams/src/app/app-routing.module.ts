13d7557834eff9f5b83c7867bf7534d1ade78749
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-21 10:52:01 +0200

    terminada la lista de jugadores

--- nba_player_teams/src/app/app-routing.module.ts
+++ nba_player_teams/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TeamsComponent} from "./components/teams/teams.component";
+/*import {TeamsDetailsComponent} from "./components/teams_details/teams_details.component";
+import {PlayersComponent} from "./components/players/players.component";*/
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', },
+/*  {path: 'players', component: PlayersComponent},*/
+  {path: 'teams', children: [
+      {path: '', component: TeamsComponent},
+/*      {path: ':id', component: TeamsDetailsComponent}*/
+    ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
