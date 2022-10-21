985680cbe7896ada3babfe16850367b46d44a1c4
AlejandroDV <aflowv@hotmail.com>
2022-10-21 17:40:03 +0200

    Components and some style finished

--- nba_player_teams/src/app/components/players/players.component.ts
+++ nba_player_teams/src/app/components/players/players.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { retry } from 'rxjs';
 import { Internal, League, Standard } from 'src/app/interfaces/players.interface';
 import { PlayersService } from 'src/app/services/players.service';
 
@@ -25,5 +26,11 @@ export class PlayersComponent implements OnInit {
     let imgUrl="https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/"+player.personId+".png";
 
     return imgUrl;
+  }
+  getPlayerAge(player: Standard){
+    let age: number;
+    var timeDiff = Math.abs(Date.now() - new Date(player.dateOfBirthUTC).getTime());
+    age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    return age;
   }
 }
