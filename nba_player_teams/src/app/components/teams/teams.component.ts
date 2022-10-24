855d42aa74287b5a2c96a0fdfbebaf8d45f9a09f
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-24 12:07:33 +0200

    hecho el select básico para cambiar año

--- nba_player_teams/src/app/components/teams/teams.component.ts
+++ nba_player_teams/src/app/components/teams/teams.component.ts
@@ -13,22 +13,37 @@ export class TeamsComponent implements OnInit {
   sacramento: Sacramento[] = [];
   vega: Vega[] = [];
   utah: Utah[] = [];
+  selected: string = '2022';
 
   constructor(private teamsService: TeamsService) {
   }
 
   ngOnInit(): void {
-    this.teamsService.getTeams('2018').subscribe(response => {
-        this.standard = response.league.standard;
-        this.africa = response.league.africa;
-        this.sacramento = response.league.sacramento;
-        this.vega = response.league.vegas;
-        this.utah = response.league.utah;
-      }
-    )
+    this.teamsService.getTeams(this.selected).subscribe(response => {
+      this.standard = response.league.standard;
+      this.africa = response.league.africa;
+      this.sacramento = response.league.sacramento;
+      this.vega = response.league.vegas;
+      this.utah = response.league.utah;
+    });
   }
 
   getTeamSVG(id: string) {
     return `https://cdn.nba.com/logos/nba/${id}/global/L/logo.svg`
+  }
+
+  reCharge() {
+    this.standard = [];
+    this.africa = [];
+    this.sacramento = [];
+    this.vega = [];
+    this.utah = [];
+    this.teamsService.getTeams(this.selected).subscribe(response => {
+      this.standard = response.league.standard;
+      this.africa = response.league.africa;
+      this.sacramento = response.league.sacramento;
+      this.vega = response.league.vegas;
+      this.utah = response.league.utah;
+    });
   }
 }
