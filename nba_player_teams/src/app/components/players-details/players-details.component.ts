2b68abbb8273607e21df92a30c23f476ed1f61fc
Alejandro Damas <alejandrodamas3d@gmail.com>
2022-10-24 13:31:18 +0200

    advanced details

--- nba_player_teams/src/app/components/players-details/players-details.component.ts
+++ nba_player_teams/src/app/components/players-details/players-details.component.ts
@@ -27,5 +27,19 @@ export class PlayersDetailsComponent implements OnInit {
       this.currentPlayer = playersArray.find(x => x.personId === this.id);
     })
   }
+  getPlayerImg(player:Standard){
+    let imgUrl="https://ak-static.cms.nba.com/wp-content/uploads/headshots/nba/latest/260x190/"+player.personId+".png";
 
-}
+    return imgUrl;
+  }
+  getPlayerAge(player: Standard){
+    let age: number;
+    var timeDiff = Math.abs(Date.now() - new Date(player.dateOfBirthUTC).getTime());
+    age = Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
+    return age;
+  }
+  getTeamSVG(idTeam: string) {
+    return `https://cdn.nba.com/logos/nba/${idTeam}/global/L/logo.svg`
+  }
+  }
+
