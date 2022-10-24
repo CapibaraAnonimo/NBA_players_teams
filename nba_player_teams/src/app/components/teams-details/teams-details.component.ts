7eaa5d4e86e08b99c26f8e0b93c1fd8a3012e0f5
CapibaraNonimo <adrianarnaizcano@gmail.com>
2022-10-24 10:33:30 +0200

    más o menos terminada la página de detalle con los jugadores

--- nba_player_teams/src/app/components/teams-details/teams-details.component.ts
+++ nba_player_teams/src/app/components/teams-details/teams-details.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {TeamsService} from "../../services/teams.service";
 import {ActivatedRoute} from "@angular/router";
 import {PlayersService} from "../../services/players.service";
+import {Standard} from "../../interfaces/players.interface";
 
 @Component({
   selector: 'app-teams-details',
@@ -9,11 +10,11 @@ import {PlayersService} from "../../services/players.service";
   styleUrls: ['./teams-details.component.css']
 })
 export class TeamsDetailsComponent implements OnInit {
-  id = -1;
+  id = '';
   found = false
   team: any;
-  players: any[] = [];
-  displayedColumns: string[] = ['firstName', 'team', 'jersey', 'pos', 'heightMeters'];
+  players!: Standard[];
+  displayedColumns: string[] = ['firstName', 'team', 'jersey', 'pos', 'heightMeters', 'weightKilograms', 'country'];
 
   constructor(private teamService: TeamsService, private playerService: PlayersService, private route: ActivatedRoute) {
   }
@@ -21,54 +22,18 @@ export class TeamsDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.id = params['id'];
+      let playersTeam: Standard[] = [];
+      this.playerService.getPlayers(2022).subscribe(response => {
+        for (let player of [...response.league.standard, ...response.league.africa, ...response.league.sacramento, ...response.league.vegas, ...response.league.utah]) {
+          if (player.teamId == this.id) {
+            playersTeam.push(player);
+          }
+        }
+        this.players = playersTeam;
+      });
     });
-    this.teamService.getTeams('2022').subscribe(response => {
-      for (let team of response.league.standard) {
-        if (team.teamId == this.id.toString()) {
-          this.team = team;
-          this.found = true;
-        }
-      }
-      if (!this.found) {
-        for (let team of response.league.africa) {
-          if (team.teamId == this.id.toString()) {
-            this.team = team;
-            this.found = true;
-          }
-        }
-        if (!this.found) {
-          for (let team of response.league.sacramento) {
-            if (team.teamId == this.id.toString()) {
-              this.team = team;
-              this.found = true;
-            }
-          }
-          if (!this.id) {
-            for (let team of response.league.vegas) {
-              if (team.teamId == this.id.toString()) {
-                this.team = team;
-                this.found = true;
-              }
-            }
-            if (!this.found) {
-              for (let team of response.league.utah) {
-                if (team.teamId == this.id.toString()) {
-                  this.team = team;
-                  this.found = true;
-                }
-              }
-            }
-          }
-        }
-      }
-    })
-    this.playerService.getPlayers(2022).subscribe(response => {
-      for (let player of [...response.league.standard, ...response.league.africa, ...response.league.sacramento, ...response.league.vegas, ...response.league.utah]) {
-        if (player.teamId == this.id.toString()) {
-          this.players.push(player);
-        }
-      }
-    })
+
+
   }
 
 }
